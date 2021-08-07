14921c6b40c059309da1cf1190b07e8e08bd1ad7
Tymur Foshch <antariuz@gmail.com>
2021-08-07 17:12:21 +0300

    done

--- src/ExerciseTwo.java
+++ src/ExerciseTwo.java
@@ -2,9 +2,7 @@ public class ExerciseTwo {
     public static void main(String[] args) {
         MatrixReverser matrixReverser = new MatrixReverser();
         int[][] arrayOne = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
-        matrixReverser.showMatrix(arrayOne);
-        System.out.println();
+        matrixReverser.showInitialMatrix(arrayOne);
         matrixReverser.solve(arrayOne);
-        matrixReverser.showMatrix(arrayOne);
     }
 }
--- src/MatrixReverser.java
+++ src/MatrixReverser.java
@@ -1,20 +1,22 @@
 public class MatrixReverser {
 
     public void solve(int[][] m) {
-        for (int i = 0; i <= Math.floorDiv(m.length, 2); i++) {
-            int temp = m[i][i];
-            m[i][i] = m[m.length - 1 - i][m.length - 1 - i];
-            m[m.length - 1 - i][m.length - 1 - i] = temp;
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                System.out.print(m[m.length - 1 -j][i] + " ");
+            }
+            System.out.println();
         }
     }
 
-    public void showMatrix(int[][] m) {
+    public void showInitialMatrix(int[][] m) {
         for (int[] ints : m) {
             for (int anInt : ints) {
                 System.out.print(anInt + " ");
             }
             System.out.println();
         }
+        System.out.println();
     }
 
 }
